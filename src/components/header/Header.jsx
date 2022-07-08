708a8885003c2ec6e541a5ed674c6edcaf4f25ca
Partyush Kumar <kumarpartyush007@gmail.com>
2022-07-08 17:53:22 +0530

    developing the header and navigation bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,35 @@
-import { AppBar, Toolbar, Box, styled} from '@mui/material';
+import { AppBar, Toolbar, Box, styled, Typography} from '@mui/material';
 
 const StyledHeader = styled(AppBar)`
 background: #2874f0;
 height: 55px;
 `
+const Component = styled(Box)`
+margin-left: 12%;
+line-height: 0;
+`
+const Subheading = styled(Typography)`
+font-size:10px;
+`
+const PlusImage = styled('img')`
+width : 10px;
+`
 const Header = () => {
     const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
+    const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png'
     return (
     <StyledHeader>
         <Toolbar>
-            <Box>
-                <img src={logoURL} alt= "logo"/>
-            </Box>
+            <Component>
+                <img src={logoURL} alt= "logo" style={{width:75}}/>
+                <Box>
+                    <Subheading>
+                        Explore&nbsp;
+                        <Box component= "span" style={{color: '#FFE500'}}>Plus</Box> 
+                    </Subheading>
+                    <PlusImage src={subURL} alt = "sub-logo"/>
+                </Box>
+            </Component>
         </Toolbar>
     </StyledHeader>
     )
