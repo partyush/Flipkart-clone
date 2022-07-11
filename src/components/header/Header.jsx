dbffc9c66ddba4db0217abd9bb592f9692da8baf
Partyush Kumar <kumarpartyush007@gmail.com>
2022-07-12 00:58:05 +0530

    created new file stucture

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import { AppBar, Toolbar, Box, styled, Typography} from '@mui/material';
 
+import Search from './Search';
+
 const StyledHeader = styled(AppBar)`
 background: #2874f0;
 height: 55px;
@@ -11,9 +13,12 @@ line-height: 0;
 const Subheading = styled(Typography)`
 font-size:10px;
 `
-const PlusImage = styled('img')`
-width : 10px;
-`
+const PlusImage = styled('img')({
+width : 10,
+height : 10, 
+marginLeft: 4
+})
+
 const Header = () => {
     const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
     const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png'
@@ -22,7 +27,7 @@ const Header = () => {
         <Toolbar>
             <Component>
                 <img src={logoURL} alt= "logo" style={{width:75}}/>
-                <Box>
+                <Box style={{display:'flex'}}>
                     <Subheading>
                         Explore&nbsp;
                         <Box component= "span" style={{color: '#FFE500'}}>Plus</Box> 
---
+++ src/components/header/Search.jsx
@@ -0,0 +1,6 @@
+const Search = () => {
+    return (
+        <p>Hello</p>
+    )
+}
+export default Search;
