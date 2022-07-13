356b687a71c37047f5dc4c296f24a5ed53f5d62a
Partyush Kumar <kumarpartyush007@gmail.com>
2022-07-13 13:15:08 +0530

    created new file stucture

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
     const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png'
     return (
     <StyledHeader>
-        <Toolbar>
+        <Toolbar style={{minHeight:55}}>
             <Component>
                 <img src={logoURL} alt= "logo" style={{width:75}}/>
                 <Box style={{display:'flex'}}>
@@ -35,6 +35,7 @@ const Header = () => {
                     <PlusImage src={subURL} alt = "sub-logo"/>
                 </Box>
             </Component>
+            <Search/>
         </Toolbar>
     </StyledHeader>
     )
--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -1,6 +1,32 @@
+import { InputBase, Box, styled} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+const SearchContainer = styled(Box)`
+background:#fff;
+width: 38%;
+border-radius: 2px;
+margin-left:10px;
+display:flex;
+
+`;
+const InputSearchBase = styled(InputBase)`
+padding-left: 20px;
+width: 100%;
+font-size: unset;
+`;
+const SearchIconWrapper = styled(Box)`
+color: blue;
+padding:5px;
+`;
+
 const Search = () => {
     return (
-        <p>Hello</p>
+        <SearchContainer><InputSearchBase 
+        placeholder='Search for products and brands and more'/>
+        <SearchIconWrapper>
+            <SearchIcon />
+        </SearchIconWrapper>
+        </SearchContainer>
+        
     )
 }
 export default Search;
